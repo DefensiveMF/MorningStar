fe0d2a827be11a5d296d1305c9c7b4f4c280149e
Rafael Canales <r.canales@prime.local>
2017-06-07 09:32:55 -0500

    agregue comandos 'lista' y '52 week lows' y try and except para las companias que fallan

--- InfoRetrievefromMorningstar_2.py
+++ InfoRetrievefromMorningstar_2.py
@@ -5,25 +5,44 @@ date=re.findall('\S*? (\S{3,}) (\d{1,2}) .+?:.+?:.+? (\d{4})',time.ctime()) #get
 month,day,year=date[0][0],date[0][1],date[0][2]
 
 print month, year
+errorlist=[]
 while True:
     company=raw_input('Enter company ID -> ')
     if len(company)<1:break
 
-    companyinfo={}
-    dictIS=DMFlib.IncomeStatement(company)
-    dictBS=DMFlib.BalanceSheet(company)
-    dictKR=DMFlib.KeyRatios(company)
-    dictionaries = [dictIS, dictBS, dictKR]
-    companyinfo['EXR']=DMFlib.ExchangeRate(dictIS['currency'])
+    list=[]
+    if company=='lista': #hidden egg command to analyze several companies from a list
+        list=DMFlib.GetListFromFile('lista')
+    elif company=='52 week lows': #hidden egg command to get 52 week lows from 24/7 wall street
+        list=DMFlib.Retrieve52WeekLows()
+    else: list.append(company)
 
-    #price and market capitalization
-    share=Share(company)
-    companyinfo['quote']=share.get_price()
-    companyinfo['MC']=float(share.get_market_cap()[:-1])
-    companyinfo['name']=share.get_name()
+    for company in list:
+        try: #some companies fail
+            print "Getting",company+'...'
+            companyinfo={}
+            dictIS=DMFlib.IncomeStatement(company)
+            dictBS=DMFlib.BalanceSheet(company)
+            dictKR=DMFlib.KeyRatios(company)
+            dictionaries = [dictIS, dictBS, dictKR]
+            companyinfo['EXR']=DMFlib.ExchangeRate(dictIS['currency'])
 
-    for dictionary in dictionaries: #merge dictionaries
-        companyinfo.update(dictionary)
+            #price and market capitalization
+            share=Share(company)
+            companyinfo['quote']=share.get_price()
+            companyinfo['MC']=float(share.get_market_cap()[:-1])
+            companyinfo['name']=share.get_name()
 
-    #write to DMF excel
-    DMFlib.WriteToExcel(company,companyinfo,month,year)
+            for dictionary in dictionaries: #merge dictionaries
+                companyinfo.update(dictionary)
+
+            DMFlib.WriteToExcel(company,companyinfo,month,year) #write to DMF excel
+        except: #if company fail print error and add it to a list for further documentation
+            print 'Error: Could not add',company
+            errorlist.append(company)
+
+errorfile=open('ErrorList.txt','w') #write the companies that couldn't be analyzed to a file in plain text
+print 'Generating error file...'
+for company in errorlist:
+    errorfile.write(company+'\n')
+print 'All Done.'
